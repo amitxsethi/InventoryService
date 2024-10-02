31688b92ff96e90641b2ea697d42bd85b6c0f607
amitsethi <amit.x.sethi@gmail.com>
2024-10-02 10:13:27 +0100

    implemented circuit breaker with resilience4j

--- src/main/java/com/example/app/client/RedisServiceClient.java
+++ src/main/java/com/example/app/client/RedisServiceClient.java
@@ -1,7 +1,12 @@
 package com.example.app.client;
 
 
+import java.util.Arrays;
+
 import com.example.app.model.Inventory;
+import com.example.app.model.Item;
+import com.example.app.model.Stock;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -17,10 +22,19 @@ public class RedisServiceClient implements ExternalServiceClient {
     RestTemplate restTemplate;
 
     @Override
+    @CircuitBreaker(name = "redisClientCircuitBreaker", fallbackMethod = "getInventoryFallBack")
     public Inventory getInventory() {
         return restTemplate.getForObject(
                 redisServiceUrl,
                 Inventory.class, getHeaders());
+    }
+
+    public Inventory getInventoryFallBack(Exception e) {
+        return new Inventory(Arrays.asList(
+                new Stock(Item.TV, 0L),
+                new Stock(Item.FRIDGE, 0L),
+                new Stock(Item.WASHING_MACHINE, 0L)
+        ));
     }
 
 }
--- src/main/java/com/example/app/service/InventoryServiceImpl.java
+++ src/main/java/com/example/app/service/InventoryServiceImpl.java
@@ -4,6 +4,9 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.Stream;
 
 import com.example.app.client.MongoServiceClient;
@@ -12,12 +15,15 @@ import com.example.app.client.RedisServiceClient;
 import com.example.app.model.Inventory;
 import com.example.app.model.Item;
 import com.example.app.model.Stock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InventoryServiceImpl implements InventoryService{
 
+    private static final Logger log = LoggerFactory.getLogger(InventoryServiceImpl.class);
     @Autowired
     RedisServiceClient redisServiceClient;
 
@@ -30,18 +36,25 @@ public class InventoryServiceImpl implements InventoryService{
 
     @Override
     public Inventory getInventory() {
+
+        log.info("Update");
         Map<Item, Long> itemToQuantityMap = new HashMap<>();
 
-        List<Stock> stocksFromRedis =
-                redisServiceClient.getInventory().getStockList();
-        List<Stock> stocksFromMongo =
-                mongoServiceClient.getInventory().getStockList();
-        List<Stock> stocksFromPostgres =
-                postgresServiceClient.getInventory().getStockList();
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            Future<List<Stock>> stocksFromRedis =
+                    executor.submit(() -> redisServiceClient.getInventory().getStockList());
+            Future<List<Stock>> stocksFromMongo =
+                    executor.submit(() -> mongoServiceClient.getInventory().getStockList());
+            Future<List<Stock>> stocksFromPostgres =
+                    executor.submit(() -> postgresServiceClient.getInventory().getStockList());
 
-        Stream.of(stocksFromRedis, stocksFromMongo, stocksFromPostgres)
-                .flatMap(Collection::stream).forEach(stock -> itemToQuantityMap.put(stock.item(),
-                        stock.quantity() + itemToQuantityMap.getOrDefault(stock.item(), 0L)));
+            Stream.of(stocksFromRedis.get(), stocksFromMongo.get(), stocksFromPostgres.get())
+                    .flatMap(Collection::stream).forEach(stock -> itemToQuantityMap.put(stock.item(),
+                            stock.quantity() + itemToQuantityMap.getOrDefault(stock.item(), 0L)));
+
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         return new Inventory(itemToQuantityMap.entrySet().stream().map(entry-> new Stock(entry.getKey(),
                 entry.getValue())).toList());
